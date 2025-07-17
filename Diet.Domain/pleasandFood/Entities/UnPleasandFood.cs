07deecc239694e264d68e31e26920d4717172803
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-17 23:42:16 +0330

    ✨ feat(food): add UnPleasandFood entity
    - introduce UnPleasandFood entity to represent disliked food items
    - implement basic properties like FoodId and navigation property to Food

---
+++ Diet.Domain/pleasandFood/Entities/UnPleasandFood.cs
@@ -0,0 +1,16 @@
+﻿using Diet.Domain.common;
+using Diet.Domain.food.Entities;
+
+namespace Diet.Domain.pleasandFood.Entities;
+
+/// <summary>
+/// غذای ناخوشایند
+/// </summary>
+public sealed class UnPleasandFood: BaseEntity
+{
+    private UnPleasandFood() { }
+
+    public Guid? FoodId { get; private set; }
+
+    public Food? Food { get; private set; }
+}
