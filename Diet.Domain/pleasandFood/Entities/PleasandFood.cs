7d44ece18062695c162b9a79d4273a36d7d365bb
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-17 23:41:37 +0330

    ✨ feat(pleasandFood): create PleasandFood entity
    - introduce PleasandFood entity to represent enjoyable food choices
    - link to Food entity via FoodId for food details

---
+++ Diet.Domain/pleasandFood/Entities/PleasandFood.cs
@@ -0,0 +1,15 @@
+﻿using Diet.Domain.common;
+using Diet.Domain.food.Entities;
+
+namespace Diet.Domain.pleasandFood.Entities;
+
+/// <summary>
+/// غذای خوشایند
+/// </summary>
+public sealed class PleasandFood : BaseEntity
+{
+    public Guid? FoodId { get; private set; }
+
+    public Food? Food { get; private set; }
+    private PleasandFood() { }
+}
