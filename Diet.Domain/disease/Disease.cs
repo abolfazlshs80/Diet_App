f664ca70ce00e4c9399e4635bded182428e3eaa9
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-17 23:39:11 +0330

    ✨ feat(disease): implement disease entity
    - add disease entity with title, parent id, and case relation
    - create supplement disease whitelist collection

--- Diet.Domain/disease/Disease.cs
+++ Diet.Domain/disease/Disease.cs
@@ -3,12 +3,16 @@ using Diet.Domain.supplement.Entities;
 
 namespace Diet.Domain.disease;
 
-public class Disease:BaseEntity
+
+/// <summary>
+/// بیماری
+/// </summary>
+public sealed class Disease:BaseEntity
 {
     public string Title { get;private set; }
     public Guid ParentId { get; private set; } = Guid.Empty;
 
-    public virtual Case.Case Case { get;private set; }
+    public  Case.Case Case { get;private set; }
 
     public ICollection<SupplementDisease_WhiteList> SupplementDisease_WhiteList { get; private set; }
 
