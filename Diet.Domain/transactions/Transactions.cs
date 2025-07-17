4fc79d8fa813c5c87181365ab69358b0242e2e6d
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-17 23:42:47 +0330

    ✨ feat(transactions): add transaction entity
    - add Transactions entity to domain
    - include properties: TotalPrice, ZarinPalAuthority, ZarinPalRefNum, TransactionType

--- Diet.Domain/transactions/Transactions.cs
+++ Diet.Domain/transactions/Transactions.cs
@@ -1,12 +1,25 @@
 ﻿using Diet.Domain.common;
+using Diet.Domain.Contract.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Diet.Domain.transactions;
-
+/// <summary>
+///  نمایانگر یک تراکنش مالی یا عملیاتی در سیستم
+/// </summary>
 public class Transactions : BaseEntity
 {
     private Transactions()
     {
 
     }
+
+    public double TotalPrice { get; private set; }
+    public string? ZarinPalAuthority { get; private set; }
+    public string? ZarinPalRefNum { get; private set; }
+    public TransactionType TransactionType { get; private set; }
+
+    ////public Guid PlanPluginId { get; set; }
+    ////[ForeignKey("PlanPluginId")]
+    ////public virtual PlanPlugin PlanPlugin { get; set; }
 }
 
