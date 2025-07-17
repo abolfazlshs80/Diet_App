c35c75f8b8079615d4b01c113819323692742fa6
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-17 23:42:39 +0330

    ✨ feat(ticket): implement ticket and message entities
    - add ticket entity with properties like title, number, priority, status, case id, user id, and messages
    - add ticketmessage entity for managing ticket-related messages, including text, file name, ticket id, and user id

--- Diet.Domain/ticket/Entities/Ticket.cs
+++ Diet.Domain/ticket/Entities/Ticket.cs
@@ -1,12 +1,35 @@
-﻿using Diet.Domain.common;
+﻿using Diet.Domain.Case;
+using Diet.Domain.common;
+using Diet.Domain.Contract.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Numerics;
 
 namespace Diet.Domain.ticket.Entities;
+/// <summary>
+/// رای ثبت و مدیریت درخواست‌ها یا مشکلات کاربران
+/// </summary>
 
-public class Ticket:BaseEntity
+public sealed class Ticket:BaseEntity
 {
     private Ticket()
     {
 
     }
+
+    public string Title { get; private set; }
+    public string Number { get; private set; }
+    public Priority Priority { get; private set; }
+    public TicketStatus Status { get; private   set; }
+
+    public Guid? CaseId { get; private set; }
+  
+    public  Case.Case? Case { get; private set; }
+
+    public string UserId { get; private set; }
+    //[ForeignKey("UserId")]
+    //public virtual ApplicationUser User { get; set; }
+
+    public  ICollection<TicketMessage> Messages { get; private set; }
+
 }
 
--- Diet.Domain/ticket/Entities/TicketMessage.cs
+++ Diet.Domain/ticket/Entities/TicketMessage.cs
@@ -1,12 +1,29 @@
 ﻿using Diet.Domain.common;
+using Diet.Domain.user;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Diet.Domain.ticket.Entities;
+/// <summary>
+///  پیام‌ها یا گفتگوهای مرتبط با یک تیکت
+/// </summary>
 
-public class TicketMessage : BaseEntity
+
+public sealed class TicketMessage : BaseEntity
 {
     private TicketMessage()
     {
         
     }
+
+    public string TextMessage { get; set; }
+    public string FileName { get; set; }
+
+    public Guid TicketId { get; set; }
+    [ForeignKey("TicketId")]
+    public  Ticket Ticket { get; set; }
+
+    public string FromId { get; set; }
+    [ForeignKey("FromId")]
+    public  User FromUser { get; set; }
 }
 
