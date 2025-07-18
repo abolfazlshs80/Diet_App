bd6fced1f0f3f89c09656a41ef7184643195f87c
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-18 16:52:59 +0330

    ✨ feat(case): add case entity with properties
    - add case properties: Description, CaseType, SportActivity, ChangeWeightType, FoodChooseType, WeightLoseType, WeightGainWithSportType, WeightGainWithoutSportType, Gender, BodyForm, lifeCourseId, LifeCourse, Drug, Disease, WeightGoal, SupplementAlerzhy, Weight, Height, Age, Lifestyle, FoodDislike, FoodLike, Job

--- Diet.Domain/case/case.cs
+++ Diet.Domain/case/case.cs
@@ -12,7 +12,7 @@ using System.Collections.ObjectModel;
 
 namespace Diet.Domain.Case;
 
-public sealed class Case :BaseEntity
+public sealed  class Case :BaseEntity
 {
 
     private Case() { }
@@ -20,14 +20,24 @@ public sealed class Case :BaseEntity
 
     public string Description { get; private  set; }
 
+    //i dont know
+    public CaseType CaseType { get; private set; }
+    public SportActivity SportActivity { get; private set; }
+    public ChangeWeightType ChangeWeightType { get; private set; }
+
+    public FoodChooseType FoodChooseType { get; private set; }
+    public WeightLoseType? WeightLoseType { get; private set; }
+    public WeightGainWithSportType? WeightGainWithSportType { get; private set; }
+    public WeightGainWithoutSportType? WeightGainWithoutSportType { get; private set; }
+
     public Gender Gender { get; private set; }
     public BodyForm BodyForm { get; private set; }
     public Guid lifeCourseId { get; private set; }
-    public Guid DiseaseId { get; private set; }
-    public  Disease Disease { get; private set; }
-    public Guid weightGoalId { get; private set; }
-    public WeightGoal weightGoal { get; private set; }
+    public LifeCourse LifeCourse { get; private set; }
+
     public ICollection<Drug> Drug { get; private set; }
+    public ICollection<Disease> Disease { get; private set; }
+    public ICollection<WeightGoal> WeightGoal { get; private set; }
     public List<Guid> SupplementAlerzhy { get; private set; }
 
     public double Weight { get; private set; }
