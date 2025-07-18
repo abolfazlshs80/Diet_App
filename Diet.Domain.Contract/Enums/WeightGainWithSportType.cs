8fcd3302ce8fcd09553dd664764da01cb5023f5f
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-18 16:53:24 +0330

    ✨ feat(enum): add WeightGainWithSportType enum
    - introduce WeightGainWithSportType enum to represent different weight gain goals with sport activities

---
+++ Diet.Domain.Contract/Enums/WeightGainWithSportType.cs
@@ -0,0 +1,10 @@
+﻿namespace Diet.Domain.Contract.Enums
+{
+    public enum WeightGainWithSportType
+    {
+        OneKilosInMonth,
+        TwoKilosInMonth,
+        ThreeKilosInMonth,
+        FourKilosInMonth
+    }
+}
