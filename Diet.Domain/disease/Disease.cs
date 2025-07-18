41534f3aa6aa003ffa8ce1cccba23dcb2913b96d
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-18 09:53:21 +0330

    ♻️ refactor(disease): remove unused case property
    - remove `Case` property from `Disease` class
    - this property was not being used and was causing unnecessary complexity

--- Diet.Domain/disease/Disease.cs
+++ Diet.Domain/disease/Disease.cs
@@ -12,7 +12,6 @@ public sealed class Disease:BaseEntity
     public string Title { get;private set; }
     public Guid ParentId { get; private set; } = Guid.Empty;
 
-    public  Case.Case Case { get;private set; }
 
     public ICollection<SupplementDisease_WhiteList> SupplementDisease_WhiteList { get; private set; }
 
