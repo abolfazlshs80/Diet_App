d04d1f7c0d46d237cb341d8c39cbd11ee16dd419
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-20 18:53:22 +0330

    ✨ feat(disease): enhance disease entity
    - add parent-child relationship for hierarchical disease structure
    - add navigation properties for accessing related entities

--- Diet.Domain/disease/Disease.cs
+++ Diet.Domain/disease/Disease.cs
@@ -1,4 +1,6 @@
-﻿using Diet.Domain.common;
+﻿using Diet.Domain.Case;
+using Diet.Domain.common;
+using Diet.Domain.Recommendation.Entities;
 using Diet.Domain.supplement.Entities;
 
 namespace Diet.Domain.disease;
@@ -10,11 +12,16 @@ namespace Diet.Domain.disease;
 public sealed class Disease:BaseEntity
 {
     public string Title { get;private set; }
-    public Guid ParentId { get; private set; } = Guid.Empty;
+    public Guid? ParentId { get; private set; } 
+    public Disease Parent { get; private set; } 
 
 
+
+
+    public ICollection<Disease> Childeren { get; private set; }
     public ICollection<SupplementDisease_WhiteList> SupplementDisease_WhiteList { get; private set; }
+    public ICollection<RecommendationDisease_WhiteList> RecommendationDisease_WhiteList { get; private set; }
 
-    public ICollection<Case.Case> Case { get; set; }
+    public ICollection<CaseDisease> CaseDisease { get; set; }
 
 }
