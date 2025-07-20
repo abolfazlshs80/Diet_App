95e09428aa92689f6df7f78d7d17e1a3fdb95e79
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-20 18:55:00 +0330

    ✨ feat(drug): add drug entity configuration
    - implement ef configuration for drug entity
    - include title, description properties
    - configure many-to-many relationship with case
    - configure one-to-many relationship with food_drug_interaction

---
+++ Diet.Persistence.EF/Configurations/Drug/DrugConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Diet.Domain.supplement.Entities;
+using Diet.Domain.user;
+using Diet.Domain.user.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Diet.Persistence.EF.Configurations.User;
+
+public class DrugConfiguration : IEntityTypeConfiguration<Drug>
+{
+    public void Configure(EntityTypeBuilder<Drug> entity)
+    {
+        entity.HasKey(e => e.Id);
+
+        entity.Property(e => e.Title)
+              .IsRequired()
+              .HasMaxLength(200);
+
+        entity.Property(e => e.Description)
+              .HasMaxLength(1000);
+
+        // رابطه Many-to-Many: Drug <-> Case
+        entity.HasMany(e => e.CaseDrug)
+              .WithOne(c => c.Drug) // ← باید در Case تعریف شده باشه
+              .HasForeignKey(fdi => fdi.DrugId)
+              .OnDelete(DeleteBehavior.Cascade);
+
+        // رابطه One-to-Many: Drug → Food_Drug_Intraction
+        entity.HasMany(e => e.Food_Drug_Intraction)
+              .WithOne(fdi => fdi.Drug)
+              .HasForeignKey(fdi => fdi.DrugId)
+              .OnDelete(DeleteBehavior.Cascade);
+
+    }
+
+}
+
+
