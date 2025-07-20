b54963c92a7fedf513e9c94d2ad4e0058a62ebdc
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-20 18:55:07 +0330

    ✨ feat(duration-age): add duration age entity configuration
    - create DurationAgeConfiguration for EF Core
    - configure primary key, title property, and relationships with SupplementDurationAge and RecommendationDurationAge

---
+++ Diet.Persistence.EF/Configurations/DurationAge/DurationAgeConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Diet.Domain.lifeCourse;
+using Diet.Domain.supplement.Entities;
+using Diet.Domain.user.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Diet.Persistence.EF.Configurations.User;
+
+public class DurationAgeConfiguration : IEntityTypeConfiguration<DurationAge>
+{
+    public void Configure(EntityTypeBuilder<DurationAge> entity)
+    {
+        entity.HasKey(e => e.Id);
+
+        entity.Property(e => e.Title)
+              .IsRequired()
+              .HasMaxLength(200);
+
+        // رابطه One-to-Many با SupplementDurationAge
+        entity.HasMany(e => e.SupplementDurationAge)
+              .WithOne(sda => sda.DurationAge)
+              .HasForeignKey(sda => sda.DurationAgeId)
+              .OnDelete(DeleteBehavior.Cascade);
+
+        // رابطه One-to-Many با RecommendationDurationAge
+        entity.HasMany(e => e.RecommendationDurationAge)
+              .WithOne(rda => rda.DurationAge)
+              .HasForeignKey(rda => rda.DurationAgeId)
+              .OnDelete(DeleteBehavior.Cascade);
+
+    }
+
+}
+
+
