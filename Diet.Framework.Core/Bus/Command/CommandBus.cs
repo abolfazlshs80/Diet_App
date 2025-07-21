dcc705e0cc1fbf7f399c2c5d13a159f29bea39a0
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-21 08:45:51 +0330

    ✨ feat(command): implement command bus for handling commands
    - add command bus class with send method
    - implement validation command handler decorator for command validation

---
+++ Diet.Framework.Core/Bus/Command/CommandBus.cs
@@ -0,0 +1,40 @@
+﻿
+
+using ErrorOr;
+using Order.Framework.Core.Decorator;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+using Order.Framework.Core.Bus;
+using Diet.Framework.Core.Errors;
+
+namespace Diet.Framework.Core.Bus;
+
+
+public class CommandBus : ICommandBus
+{
+    IServiceProvider serviceProvider;
+
+    public CommandBus(IServiceProvider serviceProvider)
+    {
+        this.serviceProvider = serviceProvider;
+    }
+
+    public async Task<ErrorOr<TCommandResult>> Send<TCommand, TCommandResult>(TCommand command)
+        where TCommand : ICommand
+        where TCommandResult : ICommandResult
+    {
+        var handler = new ValidationCommandHandlerDecorator<TCommand, TCommandResult>(serviceProvider.GetService<ICommandHandler<TCommand, TCommandResult>>()!, serviceProvider);
+        if (handler is not null)
+        {
+            var commandResult = await handler.Handle(command);
+            if (commandResult.IsError)
+                return commandResult.FirstError;
+
+            return commandResult.Value;
+        }
+
+        return BusErrors.Command.ICommandHandlerIsNull;
+    }
+}
+
