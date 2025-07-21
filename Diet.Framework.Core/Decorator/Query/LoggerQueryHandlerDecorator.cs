a2e617a7d2bebcd88d932cb1d11ac28986a78978
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-21 08:46:32 +0330

    ✨ feat(core): add validation and logging decorators for queries
    - implemented validation decorator to validate queries before handling
    - implemented logging decorator to log queries before handling

---
+++ Diet.Framework.Core/Decorator/Query/LoggerQueryHandlerDecorator.cs
@@ -0,0 +1,36 @@
+﻿
+
+using Diet.Framework.Core.Bus;
+using ErrorOr;
+using Order.Framework.Core.Bus;
+using System;
+using System.Threading.Tasks;
+
+namespace Order.Framework.Core.Decorator;
+
+public class LoggerQueryHandlerDecorator<TQuery, TQueryResult>
+      : IQueryHandler<TQuery, TQueryResult>
+       where TQuery : IQuery
+       where TQueryResult : IQueryResult
+{
+    IServiceProvider serviceProvider;
+    private readonly IQueryHandler<TQuery, TQueryResult> _handler;
+
+    public LoggerQueryHandlerDecorator(
+        IQueryHandler<TQuery, TQueryResult> handler,
+        IServiceProvider serviceProvider)
+    {
+        this.serviceProvider = serviceProvider;
+        _handler = handler;
+    }
+
+    public async Task<ErrorOr<TQueryResult>> Handle(TQuery Query)
+    {
+        Console.WriteLine("log " + Query.GetType().ToString());
+        return await _handler.Handle(Query);
+
+    }
+
+}
+
+
---
+++ Diet.Framework.Core/Decorator/Query/ValidationQueryHandlerDecorator.cs
@@ -0,0 +1,56 @@
+﻿
+
+using ErrorOr;
+using FluentValidation;
+using Order.Framework.Core.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+using Diet.Framework.Core.Bus;
+using Diet.Framework.Core.Errors;
+
+namespace Order.Framework.Core.Decorator;
+
+ 
+
+public class ValidationQueryHandlerDecorator<TQuery, TQueryResult>
+     : IQueryHandler<TQuery, TQueryResult>
+       where TQuery : IQuery
+       where TQueryResult : IQueryResult
+{
+    IServiceProvider serviceProvider;
+    private readonly IQueryHandler<TQuery, TQueryResult> _handler;
+    private IValidator<TQuery> _validators;
+
+    public ValidationQueryHandlerDecorator(
+        IQueryHandler<TQuery, TQueryResult> handler,
+        IServiceProvider serviceProvider)
+    {
+        this.serviceProvider = serviceProvider;
+        _handler = handler;
+        _validators = serviceProvider.GetService<IValidator<TQuery>>()!;
+    }
+
+    public virtual async Task<ErrorOr<TQueryResult>> Handle(TQuery Query)
+    {
+        var validationResult = await _validators.ValidateAsync(Query);
+        if (validationResult.IsValid && validationResult is not null)
+        {
+            var handler = new LoggerQueryHandlerDecorator<TQuery, TQueryResult>(_handler, this.serviceProvider);
+            if (handler is not null)
+            {
+                return await handler.Handle(Query);
+            }
+        }
+
+        var erorr = validationResult!.Errors
+            .ConvertAll(validationFailure => Error.Validation(
+                validationFailure.PropertyName,
+                validationFailure.ErrorMessage));
+        erorr.Add(BusErrors.Query.IValidationQueryHandlerIsFalse(Query));
+
+        return erorr;
+    }
+}
+
+
