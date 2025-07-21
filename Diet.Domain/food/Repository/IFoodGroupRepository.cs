a195a4fbb435f1797affa85bc4251e7b8bca1789
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-21 08:45:25 +0330

    ✨ feat(food): add IFoodGroupRepository interface - create interface IFoodGroupRepository with basic CRUD   operations for FoodGroup entity - add methods: All, ById, Save, Update, Delete

---
+++ Diet.Domain/food/Repository/IFoodGroupRepository.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+
+using Diet.Domain.food.Entities;
+
+namespace Diet.Domain.user.Repository;
+
+public interface IFoodGroupRepository
+{
+     Task<List<FoodGroup>>  All();
+    Task<FoodGroup> ById(Guid Id);
+  
+
+    Task Save(FoodGroup FoodGroup);
+    Task Update(FoodGroup FoodGroup);
+    Task Delete(FoodGroup FoodGroup);
+}
