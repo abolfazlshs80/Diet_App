62ef109e9adf497d955b1f64a1cf046349bfc603
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-21 08:45:09 +0330

    ✨ feat(food): implement create food group command handler
    - create handler to manage food group creation logic
    - save food group to repository and persist changes

---
+++ Diet.Application/UseCase/FoodGroup/Commands/Create/CreateFoodGroupCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using Diet.Domain.Contract;
+using Diet.Domain.Contract.Commands.Order.Create;
+using Diet.Domain.food.Entities;
+using Diet.Domain.user.Repository;
+using Diet.Framework.Core.Bus;
+using ErrorOr;
+
+namespace Diet.Application.UseCase.FoodGroup.Commands.Create;
+
+public class CreateFoodGroupCommandHandler : ICommandHandler<CreateFoodGroupCommand, CreateFoodGroupCommandResult>
+{
+    private readonly IFoodGroupRepository _foodGroupRepository;
+    private readonly IUnitOfWorkService _unitOfWorkService;
+
+    public CreateFoodGroupCommandHandler(IFoodGroupRepository foodGroupRepository, IUnitOfWorkService unitOfWorkService)
+    {
+        _unitOfWorkService = unitOfWorkService;
+        _foodGroupRepository = foodGroupRepository;
+    }
+ 
+
+    public async Task<ErrorOr<CreateFoodGroupCommandResult>> Handle(CreateFoodGroupCommand command)
+    {
+
+        var orderResult = Domain.food.Entities.FoodGroup.Create(command);
+        if (orderResult.IsError)
+            return orderResult.FirstError;
+
+        await _foodGroupRepository.Save(orderResult.Value!);
+        await _unitOfWorkService.SaveAsync();
+
+        return new CreateFoodGroupCommandResult("success","ok");
+    }
+}
