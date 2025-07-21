06e07e56a899426a4c7cd1436324986d4e7ca37f
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-21 08:46:51 +0330

    ✨ feat(EF): implement unit of work service
    - add UnitOfWorkService class to manage database transactions
    - implement SaveAsync method to persist changes to the database

---
+++ Diet.Persistence.EF/Repository/UnitOfWorkService.cs
@@ -0,0 +1,20 @@
+﻿using Diet.Domain.Contract;
+using Order.Persistence.EF.Context;
+
+namespace Diet.Persistence.EF.Repository
+{
+    public class UnitOfWorkService : IUnitOfWorkService
+    {
+
+        private readonly DietDbContext _dbContext;
+        public UnitOfWorkService(DietDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<bool> SaveAsync( CancellationToken ct=default)
+        {
+            var result = await _dbContext.SaveChangesAsync(ct);
+            return result > 0;
+        }
+    }
+}
