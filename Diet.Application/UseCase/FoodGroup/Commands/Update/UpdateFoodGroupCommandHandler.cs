f45df11fefcf989d7fa469a5497368252ebec20c
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-21 13:47:03 +0330

    ✨ feat(FoodGroup): implement update food group command
    - add command, handler and validator for updating food groups
    - implement logic to update existing food group in the database

---
+++ Diet.Application/UseCase/FoodGroup/Commands/Update/UpdateFoodGroupCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using Diet.Application.Execptions;
+using Diet.Domain.Contract;
+using Diet.Domain.Contract.Commands.Order.Update;
+using Diet.Domain.food.Entities;
+using Diet.Domain.user.Repository;
+using Diet.Framework.Core.Bus;
+using ErrorOr;
+using static FoodGroup.Domain.FoodGroup.Errors.DomainErrors;
+
+namespace Diet.Application.UseCase.FoodGroup.Commands.Update;
+
+public class UpdateFoodGroupCommandHandler : ICommandHandler<UpdateFoodGroupCommand, UpdateFoodGroupCommandResult>
+{
+    private readonly IFoodGroupRepository _foodGroupRepository;
+    private readonly IUnitOfWorkService _unitOfWorkService;
+
+    public UpdateFoodGroupCommandHandler(IFoodGroupRepository foodGroupRepository, IUnitOfWorkService unitOfWorkService)
+    {
+        _unitOfWorkService = unitOfWorkService;
+        _foodGroupRepository = foodGroupRepository;
+    }
+ 
+
+    public async Task<ErrorOr<UpdateFoodGroupCommandResult>> Handle(UpdateFoodGroupCommand command)
+    {
+
+        var foodGroup = await _foodGroupRepository.ById(command.Id);
+        if (foodGroup == null)
+            return FoodGroup_Error.FoodGroup_NotFount;
+
+        var result = Domain.food.Entities.FoodGroup.Update(command);
+        if (result.IsError)
+            return result.FirstError;
+
+        await _foodGroupRepository.Update(result.Value);
+        await _unitOfWorkService.SaveAsync();
+
+        return new UpdateFoodGroupCommandResult("success","ok");
+    }
+}
