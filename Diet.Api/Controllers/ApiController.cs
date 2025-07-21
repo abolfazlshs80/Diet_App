8a96af6d098fbdc11eda7c569c8d69501f09aaeb
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-21 08:44:50 +0330

    ✨ feat(api): create abstract api controller
    - introduce abstract ApiController to handle error responses consistently
    - use ErrorOr package for standardized error representation

---
+++ Diet.Api/Controllers/ApiController.cs
@@ -0,0 +1,26 @@
+﻿
+using ErrorOr;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Diet.Api.Http;
+
+namespace Diet.Api.Controllers
+{
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        protected ActionResult Problem(List<Error> errors)
+        {
+            HttpContext.Items[HttpContextItemKeys.Errors] = errors;
+
+            var firstError = errors[0];
+            if (errors.Count is 0)
+            {
+                return Problem();
+            }
+
+            return Problem(statusCode: StatusCodes.Status400BadRequest, title: firstError.Code + " => " + firstError.Description);
+        }
+    }
+}
