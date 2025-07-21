098c8345e4375687912512f743c6f86f6379e9f4
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-21 08:44:54 +0330

    ✨ feat(FoodGroup): implement create food group endpoint   - add FoodGroupController to handle food group related requests   - implement CreateFoodGroup action to create new food groups

---
+++ Diet.Api/Controllers/FoodGroupController.cs
@@ -0,0 +1,34 @@
+﻿using Diet.Domain.Contract.Commands.Order.Create;
+using Diet.Framework.Core.Bus;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Diet.Api.Controllers;
+
+[Route("FoodGroup")]
+public class FoodGroupController : ApiController
+{
+    private readonly ICommandBus _commandBus;
+  
+
+
+    public FoodGroupController(ICommandBus commandBus)
+    {
+        _commandBus = commandBus;
+    
+    }
+
+    [HttpPost(nameof(CreateFoodGroup))]
+    public async Task<IActionResult> CreateFoodGroup(CreateFoodGroupCommand request)
+    {
+       // var createFoodGroupCommand = _mapper.Map<CreateFoodGroupCommand>(request);
+         
+        var createFoodGroupResult =
+            await _commandBus.Send<CreateFoodGroupCommand, CreateFoodGroupCommandResult>(request);
+
+        return createFoodGroupResult.Match(
+             roleResult => Ok(roleResult),
+             errors => Problem(errors));
+
+    }
+}
+
