df4793f933ae58f986cb7b371eda898d9f30bf13
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-21 08:46:04 +0330

    ✨ feat(core): add command bus infrastructure
    - introduce ICommand, ICommandBus, and ICommandResult interfaces
    - define contracts for command handling and execution

---
+++ Diet.Framework.Core/Bus/Command/ICommandBus.cs
@@ -0,0 +1,14 @@
+﻿using ErrorOr;
+using Order.Framework.Core.Bus;
+using System.Threading.Tasks;
+
+namespace Diet.Framework.Core.Bus;
+
+public interface ICommandBus
+{
+    Task<ErrorOr<TCommandResult>> Send<TCommand, TCommandResult>(TCommand command)
+         where TCommand : ICommand
+         where TCommandResult : ICommandResult
+        ;
+
+}
