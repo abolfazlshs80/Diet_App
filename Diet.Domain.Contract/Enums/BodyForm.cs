ad02008a418aef89dde700092475cee2fa021582
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-21 08:52:30 +0330

    ♻️ refactor(enums): remove lifetime enum
    - lifetime enum was removed because it was not used in the project

--- Diet.Domain.Contract/Enums/BodyForm.cs
+++ Diet.Domain.Contract/Enums/BodyForm.cs
@@ -13,16 +13,4 @@ namespace Diet.Domain.Contract.Enums
         MuscleFat,
         OnlyFat
     }
-    /// <summary>
-    /// دوران زندگی
-    /// </summary>
-   public enum LifeTime 
-    { 
-       Child,//کوپک
-       Adolescents,//نوجوان
-       Young,//جوان
-       Midlife,//میانسال
-       Elderly//سالمندی
-    
-    }
 }
