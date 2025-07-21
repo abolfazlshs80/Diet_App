953b04cae3a509a1d89cc8ef22516e518bd8529f
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-21 13:47:20 +0330

    ✨ feat(food): implement food group error handling
    - add custom error definitions for food group related issues
    - include errors for incorrect food group and food group not found

---
+++ Diet.Domain/food/Errors/Errors.FoodGroup.cs
@@ -0,0 +1,26 @@
+﻿
+using ErrorOr;
+
+
+namespace FoodGroup.Domain.FoodGroup.Errors;
+
+
+public static partial class DomainErrors
+{
+    public static class FoodGroup_Error
+    {
+        public static Error FoodGroup_Incorrect => Error.Unexpected(
+            code: "FoodGroup.Incorrect",
+            description: "FoodGroup is incorrect");
+
+
+        public static Error FoodGroup_NotFount => Error.NotFound(
+         code: "FoodGroup.NotFount",
+         description: "FoodGroup is NotFount");
+
+
+    }
+
+}
+
+
