bbe77d2fa6f724645221e6eef1819674307d254f
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-21 08:45:03 +0330

    ✨ feat(application): add dependency injection for application layer - register create food group command handler and validator

---
+++ Diet.Application/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Diet.Application.UseCase.FoodGroup.Commands.Create;
+using Diet.Domain.Contract.Commands.Order.Create;
+using Diet.Framework.Core.Bus;
+using FluentValidation;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Order.Application.UseCase.Order.Commands.Create;
+
+
+namespace Diet.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(
+        this IServiceCollection services,
+        ConfigurationManager configuration)
+    {
+        services.AddScoped<ICommandHandler<CreateFoodGroupCommand, CreateFoodGroupCommandResult>, CreateFoodGroupCommandHandler>();
+        services.AddScoped<IValidator<CreateFoodGroupCommand>, CreateFoodGroupCommandValidator>();
+        
+        return services;
+    }
+}
