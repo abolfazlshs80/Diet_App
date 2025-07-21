3fe6c6928f7e9d5b0bf47ba23e1073bf1f882f6c
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-21 08:52:22 +0330

    ✨ feat(i18n): add LifeTime enum with Persian descriptions
    - introduce LifeTime enum to represent life stages
    - add Persian descriptions for each enum value

---
+++ Diet.Domain.Contract/Enums/LifeTime.cs
@@ -0,0 +1,15 @@
+﻿namespace Diet.Domain.Contract.Enums
+{
+    /// <summary>
+    /// دوران زندگی
+    /// </summary>
+    public enum LifeTime 
+    { 
+       Child,//کوپک
+       Adolescents,//نوجوان
+       Young,//جوان
+       Midlife,//میانسال
+       Elderly//سالمندی
+    
+    }
+}
