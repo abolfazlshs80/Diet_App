e6cc68811a8dd1bab8b22d8b13c80f4d30a1957e
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-21 08:46:47 +0330

    ✨ feat(food): implement food group repository
    - create FoodGroupRepository to manage food group data access
    - implement methods for retrieving, saving, updating, and deleting food groups

---
+++ Diet.Persistence.EF/Repository/FoodGroupRepository.cs
@@ -0,0 +1,51 @@
+﻿
+
+
+using Diet.Domain.food.Entities;
+using Diet.Domain.user.Repository;
+
+using Microsoft.EntityFrameworkCore;
+using Order.Persistence.EF.Context;
+
+namespace Diet.Persistence.EF.Repository;
+
+public class FoodGroupRepository : IFoodGroupRepository
+{
+
+    private readonly DietDbContext _dbContext;
+
+    public FoodGroupRepository(DietDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<FoodGroup> ById(Guid Id)
+    {
+        return await _dbContext.FoodGroup.SingleOrDefaultAsync(x => x.Id == Id);
+    }
+
+    public async Task Save(FoodGroup FoodGroup)
+    {
+        await _dbContext.FoodGroup.AddAsync(FoodGroup);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task Update(FoodGroup FoodGroup)
+    {
+        _dbContext.Update(FoodGroup);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task Delete(FoodGroup FoodGroup)
+    {
+        _dbContext.Remove(FoodGroup);
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task<List<FoodGroup>> All()
+    {
+        return await _dbContext.FoodGroup.ToListAsync();
+    }
+
+   
+}
