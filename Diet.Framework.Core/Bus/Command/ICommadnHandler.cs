086f18c6be292f03aa10ef7846ea177b46fd4950
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-21 08:45:55 +0330

    ✨ feat(command): add ICommandHandler interface
    - introduce ICommandHandler interface for handling commands and returning command results
    - use ErrorOr for result handling

---
+++ Diet.Framework.Core/Bus/Command/ICommadnHandler.cs
@@ -0,0 +1,13 @@
+﻿using ErrorOr;
+using Order.Framework.Core.Bus;
+using System.Threading.Tasks;
+
+namespace Diet.Framework.Core.Bus;
+
+public interface ICommandHandler<TCommand, TCommandResult>
+     where TCommand : ICommand
+     where TCommandResult : ICommandResult
+{
+    Task<ErrorOr<TCommandResult>>  Handle(TCommand command);
+
+}
