56aa32b5284c24a2ede7f6cad8d8242a28d68849
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-21 13:47:15 +0330

    ✨ feat(food): implement update food group command
    - add update method for food group entity
    - create new constructor with id for update command

--- Diet.Domain/food/Entities/FoodGroup.cs
+++ Diet.Domain/food/Entities/FoodGroup.cs
@@ -1,5 +1,6 @@
 ﻿using Diet.Domain.common;
 using Diet.Domain.Contract.Commands.Order.Create;
+using Diet.Domain.Contract.Commands.Order.Update;
 using Diet.Domain.supplement;
 using ErrorOr;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -9,7 +10,9 @@ namespace Diet.Domain.food.Entities;
 
 public sealed class FoodGroup : BaseEntity
 {
-    private FoodGroup(string title):base() { Title = title; }
+    private FoodGroup(Guid id, string title):base(id) { Title = title; }
+    private FoodGroup( string title) { Title = title; }
+    
     public string Title { get; private set; }
 
     public ICollection<Food> Food { get;private set; }
@@ -19,9 +22,16 @@ public sealed class FoodGroup : BaseEntity
 
     public static ErrorOr<FoodGroup> Create(CreateFoodGroupCommand command)
     {
-        var foodGroupId = Guid.NewGuid().ToString();
 
-        var foodGroup = new FoodGroup( command.Title);
+        var foodGroup = new FoodGroup(command.Title);
+
+
+        return foodGroup;
+    }
+
+    public static ErrorOr<FoodGroup> Update(UpdateFoodGroupCommand command)
+    {
+      var  foodGroup= new FoodGroup(command.Id,command.Title);
 
 
         return foodGroup;
