2131f92b4d7d9d375bb74d124714dbb4f3490125
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-21 13:46:55 +0330

    ✨ feat(foodgroup): add delete foodgroup command validator
    - add validator for delete foodgroup command to ensure id is not null

---
+++ Diet.Application/UseCase/FoodGroup/Commands/Delete/DeleteFoodGroupCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using Diet.Domain.Contract.Commands.Order.Delete;
+using FluentValidation;
+
+namespace Diet.Application.UseCase.FoodGroup.Commands.Delete;
+
+public class DeleteFoodGroupCommandValidator : AbstractValidator<DeleteFoodGroupCommand>
+{
+    public DeleteFoodGroupCommandValidator()
+    {
+        RuleFor(x => x.Id).NotNull();
+
+    }
+}
+
