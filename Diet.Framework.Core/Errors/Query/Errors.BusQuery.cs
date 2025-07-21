4543ef020e6623371b85d069d1ad0d7505e8e0f1
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-21 08:46:38 +0330

    ✨ feat(core): implement bus error handling
    - add error handling for command and query buses
    - create specific errors for null handlers, validation failures, and authorization denials

---
+++ Diet.Framework.Core/Errors/Query/Errors.BusQuery.cs
@@ -0,0 +1,23 @@
+﻿using Diet.Framework.Core.Bus;
+using ErrorOr;
+using Order.Framework.Core.Bus;
+
+namespace Diet.Framework.Core.Errors;
+
+public static partial class BusErrors 
+{
+    public static class Query
+    {
+        public static Error IQueryHandlerIsNull => Error.Unexpected(
+            code: "IQueryHandler.Null",
+            description: "IQueryHandler Is Null");
+
+        public static Error IValidationQueryHandlerIsFalse(IQuery Query) => Error.Unexpected(
+           code: "IValidationQueryHandler.False",
+           description: "" + Query.GetType().ToString() + " Validation Is False");
+        public static Error IAuthorizeQueryHandlerDeny(IQuery Query) => Error.Unexpected(
+         code: "IAuthorizeQueryHandler.Deny",
+         description: "" + Query.GetType().ToString() + " Authorize Is Deny");
+
+    }
+}
