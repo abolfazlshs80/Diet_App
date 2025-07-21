58d5d5ed56fc9c892db849012ea92f386eaf5681
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-21 08:45:17 +0330

    ✨ feat(food): implement food group entity creation logic
    - add factory method for creating FoodGroup instances
    - ensure immutability of Title property by setting private set
    - integrate ErrorOr for handling potential creation errors

--- Diet.Domain/food/Entities/FoodGroup.cs
+++ Diet.Domain/food/Entities/FoodGroup.cs
@@ -1,14 +1,29 @@
 ﻿using Diet.Domain.common;
+using Diet.Domain.Contract.Commands.Order.Create;
 using Diet.Domain.supplement;
+using ErrorOr;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
 
 namespace Diet.Domain.food.Entities;
 
 public sealed class FoodGroup : BaseEntity
 {
+    private FoodGroup(string title):base() { Title = title; }
     public string Title { get; private set; }
 
     public ICollection<Food> Food { get;private set; }
 
-    private FoodGroup(){}
+
+
+
+    public static ErrorOr<FoodGroup> Create(CreateFoodGroupCommand command)
+    {
+        var foodGroupId = Guid.NewGuid().ToString();
+
+        var foodGroup = new FoodGroup( command.Title);
+
+
+        return foodGroup;
+    }
 }
