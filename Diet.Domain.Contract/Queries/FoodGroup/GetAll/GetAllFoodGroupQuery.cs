aff96dba63e167bf92990090153d48065b1cad46
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-21 13:47:50 +0330

    ✨ feat(foodgroup): implement get all food group query
    - add query and result for retrieving all food groups
    - include pagination and search functionality

---
+++ Diet.Domain.Contract/Queries/FoodGroup/GetAll/GetAllFoodGroupQuery.cs
@@ -0,0 +1,7 @@
+﻿using Diet.Domain.Contract.DTOs;
+using Diet.Framework.Core.Bus;
+
+namespace Diet.Domain.Contract.Queries.FoodGroup.GetAll;
+
+public record GetAllFoodGroupQuery(string? search, int CurrentPage=0, int PageSize=8):  IQuery;
+
---
+++ Diet.Domain.Contract/Queries/FoodGroup/GetAll/GetAllFoodGroupQueryResult.cs
@@ -0,0 +1,7 @@
+﻿using Order.Framework.Core.Bus;
+
+namespace Diet.Domain.Contract.Queries.FoodGroup.GetAll;
+
+public record GetAllFoodGroupQueryResult(int TotalRecords, List<GetFoodGroupItem> Data, int CurrentPage, int PageSize): IQueryResult;
+public record GetFoodGroupItem(Guid Id,string Title);
+
