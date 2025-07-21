431b37ea172e06e80a01d440bf13f25d132f04bd
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-21 13:46:46 +0330

    ✨ feat(foodgroup): implement delete food group command handler - add handler to delete food groups by id - return error if food group not found - save changes to the database

---
+++ Diet.Application/UseCase/FoodGroup/Commands/Delete/DeleteFoodGroupCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using Diet.Application.Execptions;
+using Diet.Domain.Contract;
+using Diet.Domain.Contract.Commands.Order.Delete;
+using Diet.Domain.food.Entities;
+using Diet.Domain.user.Repository;
+using Diet.Framework.Core.Bus;
+using ErrorOr;
+using static FoodGroup.Domain.FoodGroup.Errors.DomainErrors;
+
+namespace Diet.Application.UseCase.FoodGroup.Commands.Delete;
+
+public class DeleteFoodGroupCommandHandler : ICommandHandler<DeleteFoodGroupCommand, DeleteFoodGroupCommandResult>
+{
+    private readonly IFoodGroupRepository _foodGroupRepository;
+    private readonly IUnitOfWorkService _unitOfWorkService;
+
+    public DeleteFoodGroupCommandHandler(IFoodGroupRepository foodGroupRepository, IUnitOfWorkService unitOfWorkService)
+    {
+        _unitOfWorkService = unitOfWorkService;
+        _foodGroupRepository = foodGroupRepository;
+    }
+
+
+    public async Task<ErrorOr<DeleteFoodGroupCommandResult>> Handle(DeleteFoodGroupCommand command)
+    {
+        var result = await _foodGroupRepository.ById(command.Id);
+        if (result == null)
+            return FoodGroup_Error.FoodGroup_NotFount;
+
+        await _foodGroupRepository.Delete(result);
+        await _unitOfWorkService.SaveAsync();
+
+        return new DeleteFoodGroupCommandResult("success", "ok");
+    }
+}
