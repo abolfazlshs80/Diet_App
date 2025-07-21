4087fa4fd106b838d51191874da7fcffb661c2e0
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-21 13:47:55 +0330

    ✨ feat(foodgroup): implement get by id query
    - add `GetByIdFoodGroupQuery` to retrieve a food group by its ID
    - add `GetByIdFoodGroupQueryResult` to return the food group's ID and title

---
+++ Diet.Domain.Contract/Queries/FoodGroup/GetById/GetByIdFoodGroupQuery.cs
@@ -0,0 +1,6 @@
+﻿using Diet.Framework.Core.Bus;
+
+namespace Diet.Domain.Contract.Queries.FoodGroup.GetById;
+
+public record GetByIdFoodGroupQuery(Guid Id) : IQuery;
+ 
---
+++ Diet.Domain.Contract/Queries/FoodGroup/GetById/GetByIdFoodGroupQueryResult.cs
@@ -0,0 +1,5 @@
+﻿using Order.Framework.Core.Bus;
+
+namespace Diet.Domain.Contract.Queries.FoodGroup.GetById;
+
+public record GetByIdFoodGroupQueryResult(Guid Id,string Title) : IQueryResult;
