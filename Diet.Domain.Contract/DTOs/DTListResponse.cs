ea9f8b6713bc9ae302c0933d8cac08677b8f1f14
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-21 13:47:43 +0330

    ✨ feat(dto): implement generic list response DTO
    - create ListResponseDto to encapsulate paginated data
    - create ListRequestDto to standardize list requests

---
+++ Diet.Domain.Contract/DTOs/DTListResponse.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diet.Domain.Contract.DTOs
+{
+
+    public record ListResponseDto<T>(int TotalRecords, T Data, int CurrentPage, int PageSize);
+
+
+    public record ListRequestDto(string? search, int CurrentPage, int PageSize);
+}
