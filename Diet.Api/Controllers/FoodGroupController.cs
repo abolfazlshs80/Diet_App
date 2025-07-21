5fda55d0b1657c35e8fa62a0e928f08b40f19aa1
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-21 13:46:26 +0330

    ✨ feat(FoodGroup): add endpoints for CRUD operations
    - add create, update, delete endpoints for food groups
    - implement get by id and get all endpoints with optional search and pagination

--- Diet.Api/Controllers/FoodGroupController.cs
+++ Diet.Api/Controllers/FoodGroupController.cs
@@ -1,4 +1,8 @@
 ﻿using Diet.Domain.Contract.Commands.Order.Create;
+using Diet.Domain.Contract.Commands.Order.Delete;
+using Diet.Domain.Contract.Commands.Order.Update;
+using Diet.Domain.Contract.Queries.FoodGroup.GetAll;
+using Diet.Domain.Contract.Queries.FoodGroup.GetById;
 using Diet.Framework.Core.Bus;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,13 +12,14 @@ namespace Diet.Api.Controllers;
 public class FoodGroupController : ApiController
 {
     private readonly ICommandBus _commandBus;
+    private readonly IQueryBus _queryBus;
   
 
 
-    public FoodGroupController(ICommandBus commandBus)
+    public FoodGroupController(ICommandBus commandBus, IQueryBus queryBus)
     {
         _commandBus = commandBus;
-    
+        _queryBus = queryBus;
     }
 
     [HttpPost(nameof(CreateFoodGroup))]
@@ -24,6 +29,57 @@ public class FoodGroupController : ApiController
          
         var createFoodGroupResult =
             await _commandBus.Send<CreateFoodGroupCommand, CreateFoodGroupCommandResult>(request);
+
+        return createFoodGroupResult.Match(
+             roleResult => Ok(roleResult),
+             errors => Problem(errors));
+
+    }
+
+    [HttpPut(nameof(UpdateFoodGroup))]
+    public async Task<IActionResult> UpdateFoodGroup(UpdateFoodGroupCommand request)
+    {
+        var createFoodGroupResult =
+            await _commandBus.Send<UpdateFoodGroupCommand, UpdateFoodGroupCommandResult>(request);
+
+        return createFoodGroupResult.Match(
+             roleResult => Ok(roleResult),
+             errors => Problem(errors));
+
+    }
+
+    [HttpDelete(nameof(DeleteFoodGroup))]
+    public async Task<IActionResult> DeleteFoodGroup(DeleteFoodGroupCommand request)
+    {
+
+        var createFoodGroupResult =
+            await _commandBus.Send<DeleteFoodGroupCommand, DeleteFoodGroupCommandResult>(request);
+
+        return createFoodGroupResult.Match(
+             roleResult => Ok(roleResult),
+             errors => Problem(errors));
+
+    }
+
+    [HttpGet(nameof(GetFoodGroup))]
+    public async Task<IActionResult> GetFoodGroup(Guid Id)
+    {
+
+        var createFoodGroupResult =
+            await _queryBus.Send<GetByIdFoodGroupQuery, GetByIdFoodGroupQueryResult>(new GetByIdFoodGroupQuery (Id));
+
+        return createFoodGroupResult.Match(
+             roleResult => Ok(roleResult),
+             errors => Problem(errors));
+
+    }
+
+    [HttpGet(nameof(GetAllFoodGroup))]
+    public async Task<IActionResult> GetAllFoodGroup(string? search, int CurrentPage = 0, int PageSize = 8)
+    {
+
+        var createFoodGroupResult =
+            await _queryBus.Send<GetAllFoodGroupQuery, GetAllFoodGroupQueryResult>(new GetAllFoodGroupQuery(search,CurrentPage,PageSize));
 
         return createFoodGroupResult.Match(
              roleResult => Ok(roleResult),
