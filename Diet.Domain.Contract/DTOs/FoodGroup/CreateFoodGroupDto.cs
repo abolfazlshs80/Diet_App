284ff527845c7fea7abfc896ea89248f904377c2
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-23 21:01:16 +0330

    ✨ feat(foodgroup): implement foodgroup dtos
    - add dtos for create, update, delete, get and getall foodgroup operations

---
+++ Diet.Domain.Contract/DTOs/FoodGroup/CreateFoodGroupDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diet.Domain.Contract.DTOs.FoodGroup
+{
+    public record CreateFoodGroupDto(string Title);
+    public record UpdateFoodGroupDto(Guid Id,string Title);
+    public record DeleteFoodGroupDto(Guid Id);
+    public record GetFoodGroupDto(Guid Id);
+    public record GetAllFoodGroupDto(string? search, int CurrentPage = 0, int PageSize = 8);
+
+
+}
