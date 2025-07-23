14c3e1ffabfcb45d865edc5e7a49335159cf73ae
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-23 21:00:56 +0330

    ✨ feat(foodgroup): add mapping configuration for foodgroup
    - create createfoodgroupmappingconfig.cs to register mapster configurations
    - add mapping for create, delete, update commands and getall, getbyid queries

---
+++ Diet.Api/Mapping/Food/CreateFoodGroupMappingConfig.cs
@@ -0,0 +1,24 @@
+﻿
+using Diet.Domain.Contract.Commands.Order.Create;
+using Diet.Domain.Contract.Commands.Order.Delete;
+using Diet.Domain.Contract.Commands.Order.Update;
+using Diet.Domain.Contract.DTOs.FoodGroup;
+using Diet.Domain.Contract.Queries.FoodGroup.GetAll;
+using Diet.Domain.Contract.Queries.FoodGroup.GetById;
+using Mapster;
+
+namespace FoodGroup.Api.Mapping.FoodGroup;
+
+public class CreateFoodGroupMappingConfig : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        //config.NewConfig<CreateFoodGroupDto, CreateFoodGroupCommand>();
+        //config.NewConfig<DeleteFoodGroupDto, DeleteFoodGroupCommand>();
+        //config.NewConfig<UpdateFoodGroupDto, UpdateFoodGroupCommand>();
+        //config.NewConfig<GetAllFoodGroupDto, GetAllFoodGroupQuery>();
+        //config.NewConfig<GetByIdFoodGroupDto, GetByIdFoodGroupQuery>();
+        
+    }
+}
+
