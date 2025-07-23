dde30b31382f0f945380f4ae8fddcd846c250b34
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-23 21:29:59 +0330

    ♻️ refactor(food): rename methods in IFoodGroupRepository
    - rename All to AllAsync
    - rename ById to ByIdAsync
    - rename Save to AddAsync
    - rename Update to UpdateAsync
    - rename Delete to DeleteAsync

--- Diet.Domain/food/Repository/IFoodGroupRepository.cs
+++ Diet.Domain/food/Repository/IFoodGroupRepository.cs
@@ -8,11 +8,11 @@ namespace Diet.Domain.user.Repository;
 
 public interface IFoodGroupRepository
 {
-     Task<List<Domain.food.Entities. FoodGroup>>  All(string? searchText,int pageCount=8,int PageNumber=0);
-    Task<Domain.food.Entities.FoodGroup> ById(Guid Id);
+     Task<List<Domain.food.Entities. FoodGroup>>  AllAsync(string? searchText,int pageCount=8,int PageNumber=0);
+    Task<Domain.food.Entities.FoodGroup> ByIdAsync(Guid Id);
   
 
-    Task Save(Domain.food.Entities.FoodGroup FoodGroup);
-    Task Update(Domain.food.Entities.FoodGroup FoodGroup);
-    Task Delete(Domain.food.Entities.FoodGroup FoodGroup);
+    Task AddAsync(Domain.food.Entities.FoodGroup FoodGroup);
+    Task UpdateAsync(Domain.food.Entities.FoodGroup FoodGroup);
+    Task DeleteAsync(Domain.food.Entities.FoodGroup FoodGroup);
 }
