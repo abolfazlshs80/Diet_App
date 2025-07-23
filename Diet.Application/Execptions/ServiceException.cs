3b91ee0b2c0c70634febfd439ce17a6a43ddea45
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-23 21:29:10 +0330

    ✨ feat(exception): implement custom service exception
    - create `ServiceException` class for handling application-specific errors
    - add properties for type, title, status code, and error list
    - include static factory method for creating instances
    - implement `ToString` method for error representation

---
+++ Diet.Application/Execptions/ServiceException.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Diet.Application.Execptions
+{
+    public class ServiceException : Exception
+    {
+        // سازنده خصوصی
+        private ServiceException(string type, string title, string statusCode, IReadOnlyList<string>? errors)
+            : base(message: "request failed")
+        {
+            Type = type;
+            Title = title;
+            StatusCode = statusCode;
+            Errors = errors;
+        }
+
+        // خصوصیات فقط خواندنی
+        public string Type { get; }
+        public string Title { get; }
+        public string StatusCode { get; }
+        public IReadOnlyList<string>? Errors { get; }
+
+        // متد استاتیک برای ایجاد نمونه
+        public static ServiceException Create(
+            string type,
+            string title,
+            string statusCode,
+            IReadOnlyList<string>? errors = null)
+        {
+            return new ServiceException(type, title, statusCode, errors);
+        }
+
+        // متد برای نمایش اطلاعات خطا به صورت رشته
+        public override string ToString()
+        {
+            return $"Type: {Type}\nTitle: {Title}\nDetails: {StatusCode}";
+        }
+    }
+}
