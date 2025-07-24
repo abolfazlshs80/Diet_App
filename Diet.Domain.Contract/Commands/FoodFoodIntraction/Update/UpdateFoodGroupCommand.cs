8cebd3b0429786267da25b9a363de78f4b0ea0f5
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-24 15:29:28 +0330

    ✨ feat(food): add food commands
    - add commands for food interaction
      - create food interaction
      - delete food interaction
      - update food interaction
    - add commands for food stuff
      - create food stuff
      - delete food stuff
      - update food stuff

---
+++ Diet.Domain.Contract/Commands/FoodFoodIntraction/Update/UpdateFoodGroupCommand.cs
@@ -0,0 +1,8 @@
+﻿
+
+using Diet.Framework.Core.Bus;
+
+namespace Diet.Domain.Contract.Commands.Order.Update;
+
+public record UpdateFoodFoodIntractionCommand(Guid Id, Guid FoodFistId, Guid FoodSecondId) : ICommand;
+ 
---
+++ Diet.Domain.Contract/Commands/FoodStuff/Update/UpdateFoodGroupCommand.cs
@@ -0,0 +1,8 @@
+﻿
+
+using Diet.Framework.Core.Bus;
+
+namespace Diet.Domain.Contract.Commands.Order.Update;
+
+public record UpdateFoodStuffCommand(Guid Id,string Title) : ICommand;
+ 
