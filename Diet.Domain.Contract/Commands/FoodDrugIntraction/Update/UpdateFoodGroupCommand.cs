753cef0dcab8667bf70a19902a313ecaeaa23042
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-24 15:29:09 +0330

    ✨ feat(food-drug-intraction): add commands for food-drug interactions
    - add create, update, and delete commands for food-drug interactions
    - define command and result records for each operation

---
+++ Diet.Domain.Contract/Commands/FoodDrugIntraction/Update/UpdateFoodGroupCommand.cs
@@ -0,0 +1,8 @@
+﻿
+
+using Diet.Framework.Core.Bus;
+
+namespace Diet.Domain.Contract.Commands.Order.Update;
+
+public record UpdateFoodDrugIntractionCommand(Guid Id,Guid FoodId,Guid DrugId) : ICommand;
+ 
