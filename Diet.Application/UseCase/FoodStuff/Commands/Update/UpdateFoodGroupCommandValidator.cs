3ffc5c7fd831428a44776df85daa8654a55bd898
abolfazlshs80 <abolfazlshs80@gmail.com>
2025-07-24 15:27:52 +0330

    ✨ feat(foodstuff): add update foodstuff command validator
    - implements validation rules for updating foodstuff
    - ensures id is not null and title length is within range

---
+++ Diet.Application/UseCase/FoodStuff/Commands/Update/UpdateFoodGroupCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿
+
+using Diet.Domain.Contract.Commands.Order.Update;
+using FluentValidation;
+
+namespace Order.Application.UseCase.Order.Commands.Update;
+
+public class UpdateFoodStuffCommandValidator : AbstractValidator<UpdateFoodStuffCommand>
+{
+    public UpdateFoodStuffCommandValidator()
+    {
+        RuleFor(x => x.Id).NotNull();
+        RuleFor(x => x.Title).Length(2, 150).WithMessage("Please  Enter a title");
+
+    }
+}
+
